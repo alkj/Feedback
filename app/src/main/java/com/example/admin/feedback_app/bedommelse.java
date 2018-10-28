04162994085842e27613eb80e05de9ea66d1d47a
Ahad2906 <s175128@student.dtu.dk>
2018-10-28 23:34:07 +0100

    Tilføjet constraints

---
+++ app/src/main/java/com/example/admin/feedback_app/bedommelse.java
@@ -0,0 +1,13 @@
+package com.example.admin.feedback_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class bedommelse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_akt);
+    }
+}
