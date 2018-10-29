1a30b41c57489e4083c1255010ef889078b5d9fd
Nils Rasamoel <nils.rasamoel@gmail.com>
2018-10-29 18:47:18 +0100

    Mødeoversigt skift mellem de to indre fragmenter

--- app/src/main/java/com/example/admin/feedback_app/Moedeoversigt_frg.java
+++ app/src/main/java/com/example/admin/feedback_app/Moedeoversigt_frg.java
@@ -1,64 +1,25 @@
 package com.example.admin.feedback_app;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link Moedeoversigt_frg.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link Moedeoversigt_frg#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class Moedeoversigt_frg extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class Moedeoversigt_frg extends Fragment implements View.OnClickListener {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    LinearLayout afholdte_btn, ikkeAfholdte_btn;
+    Drawable selected_bg, unselected_bg;
+    FragmentManager fragmentManager;
 
-    private OnFragmentInteractionListener mListener;
-
-    public Moedeoversigt_frg() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Moedeoversigt_frg.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Moedeoversigt_frg newInstance(String param1, String param2) {
-        Moedeoversigt_frg fragment = new Moedeoversigt_frg();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -67,42 +28,45 @@ public class Moedeoversigt_frg extends Fragment {
         return inflater.inflate(R.layout.fragment_moedeoversigt, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState){
+        fragmentManager = getChildFragmentManager();
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.moedeFragHolder, new IkkeAfholdt_frg());
+        fragmentTransaction.commit();
+
+        afholdte_btn = (LinearLayout)view.findViewById(R.id.moede_afholdt_btn);
+        ikkeAfholdte_btn = (LinearLayout)view.findViewById(R.id.moede_ikkeAfholdt_btn);
+
+        afholdte_btn.setOnClickListener(this);
+        ikkeAfholdte_btn.setOnClickListener(this);
+
+        Resources res = getResources();
+        selected_bg = res.getDrawable(R.drawable.moeder_tab_selected_bg, null);
+        unselected_bg = res.getDrawable(R.drawable.moeder_tab_unselected_bg, null);
     }
 
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+    public void onClick(View view) {
+
+        //Checker før jeg begynder at lave en fragmentTransaction.
+        if (view instanceof LinearLayout){
+
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+            if (view == ikkeAfholdte_btn) {
+                afholdte_btn.setBackground(unselected_bg);
+                ikkeAfholdte_btn.setBackground(selected_bg);
+                fragmentTransaction.replace(R.id.moedeFragHolder, new IkkeAfholdt_frg());
+
+            }
+            else if (view == afholdte_btn) {
+                afholdte_btn.setBackground(selected_bg);
+                ikkeAfholdte_btn.setBackground(unselected_bg);
+                fragmentTransaction.replace(R.id.moedeFragHolder, new Afholdt_frg());
+            }
+            fragmentTransaction.commit();
         }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
     }
 }
