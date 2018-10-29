f70df9e6ed19b580962ae987af128a1ff37228a1
Ahad2906 <s175128@student.dtu.dk>
2018-10-29 08:09:29 +0100

    Tilføjet filer til drawable

--- app/src/main/java/com/example/admin/feedback_app/StartScreenActivity.java
+++ app/src/main/java/com/example/admin/feedback_app/StartScreenActivity.java
@@ -2,12 +2,19 @@ package com.example.admin.feedback_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartScreenActivity extends AppCompatActivity {
+
+    Button knap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        knap = findViewById(R.id.LoginButtonStartscreen);
     }
-}
+ }
