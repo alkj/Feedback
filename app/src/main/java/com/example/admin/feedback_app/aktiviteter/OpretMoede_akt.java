1fc95818ab27f23ba572237f0966c392de251442
Nils Rasamoel <nils.rasamoel@gmail.com>
2018-11-08 13:28:29 +0100

    Tilføjet TimePicker til opretmoeder

--- app/src/main/java/com/example/admin/feedback_app/aktiviteter/OpretMoede_akt.java
+++ app/src/main/java/com/example/admin/feedback_app/aktiviteter/OpretMoede_akt.java
@@ -1,18 +1,40 @@
 package com.example.admin.feedback_app.aktiviteter;
 
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.admin.feedback_app.R;
+import com.example.admin.feedback_app.fragmenter.TidPickerDialog_frg;
 
 
-public class OpretMoede_akt extends AppCompatActivity {
+public class OpretMoede_akt extends FragmentActivity implements View.OnClickListener {
+
+    TextView tid_txt;
 
     @Override
     protected void onCreate(Bundle savedInstancesBundle) {
         super.onCreate(savedInstancesBundle);
 
         setContentView(R.layout.activity_opret_moede);
+
+        tid_txt = findViewById(R.id.opretmoeder_tid_txt);
+
+        tid_txt.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.opretmoeder_tid_txt:
+                DialogFragment dialog = new TidPickerDialog_frg();
+                dialog.show(getSupportFragmentManager(), "tidPicker");
+                break;
+
+        }
+    }
 }
