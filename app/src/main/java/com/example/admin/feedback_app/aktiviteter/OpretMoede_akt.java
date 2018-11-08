ebfe1758117d026676ed9da5d1f6a396dd115714
Nils Rasamoel <nils.rasamoel@gmail.com>
2018-11-08 12:38:08 +0100

    Opdateret navigationen

---
+++ app/src/main/java/com/example/admin/feedback_app/aktiviteter/OpretMoede_akt.java
@@ -0,0 +1,18 @@
+package com.example.admin.feedback_app.aktiviteter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.admin.feedback_app.R;
+
+
+public class OpretMoede_akt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstancesBundle) {
+        super.onCreate(savedInstancesBundle);
+
+        setContentView(R.layout.activity_opret_moede);
+    }
+
+}
