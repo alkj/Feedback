9b87dfb32803ad483eab57caa45d2850e1d5caa0
Nicolai Dam <nicolaidam96@gmail.com>
2018-11-19 10:56:19 +0100

    login virker med gg@gmail.com og kode gggggg

--- app/src/main/java/com/example/admin/feedback_app/aktiviteter/Login_akt.java
+++ app/src/main/java/com/example/admin/feedback_app/aktiviteter/Login_akt.java
@@ -1,23 +1,37 @@
 package com.example.admin.feedback_app.aktiviteter;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.admin.feedback_app.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login_akt extends AppCompatActivity implements View.OnClickListener {
 
+    private static final String TAG = "login";
+
     private Button login_btn, nyBruger_btn, tilbage_btn;
-    private EditText brugernavn_editTxt, password_editTxt;
+    private EditText email_editTxt, password_editTxt;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
 
         //Knapper
         login_btn = (Button)findViewById(R.id.login_login_btn);
@@ -29,7 +43,7 @@ public class Login_akt extends AppCompatActivity implements View.OnClickListener
         tilbage_btn.setOnClickListener(this);
 
         //Input felter
-        brugernavn_editTxt = (EditText)findViewById(R.id.login_brugernavn_editTxt);
+        email_editTxt = (EditText)findViewById(R.id.login_brugernavn_editTxt);
         password_editTxt = (EditText)findViewById(R.id.login_password_editTxt);
 
     }
@@ -37,9 +51,12 @@ public class Login_akt extends AppCompatActivity implements View.OnClickListener
     @Override
     public void onClick(View view) {
         if (view == login_btn){
-            //TODO: håndtere login
-            Intent intent = new Intent(this, Navigation_akt.class);
-            startActivity(intent);
+            signIn(email_editTxt.getText().toString(),password_editTxt.getText().toString());
+
+            if(mAuth.getCurrentUser()!=null) {
+                Intent intent = new Intent(this, Navigation_akt.class);
+                startActivity(intent);
+            }
         }
         else if (view == nyBruger_btn){
             //Åbner opret bruger aktiviteten
@@ -51,4 +68,75 @@ public class Login_akt extends AppCompatActivity implements View.OnClickListener
             finish();
         }
     }
+
+    private void signIn(String email, String password) {
+        Log.d(TAG, "signIn:" + email);
+
+        if (!valideringEmail()) {
+            return;
+        }
+
+        //TODO lav loading her showProgressDialog();
+
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            guiLogind();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(Login_akt.this, "Forkert email og/eller password.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        //TODO fjern loading her hideProgressDialog();
+                    }
+                });
+        // [END sign_in_with_email]
+    }
+
+    public boolean valideringEmail(){
+
+        Log.d(TAG, "valideringEmail: startes");
+        boolean valid = true;
+
+        String email = this.email_editTxt.getText().toString();
+        String password = this.password_editTxt.getText().toString();
+
+        if(TextUtils.isEmpty(email) && TextUtils.isEmpty(password)){
+            this.password_editTxt.setError("Der skal indtastes et password");
+            this.email_editTxt.setError("Der skal indtastes en E-mail.");
+            valid = false;
+        }
+        if (TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)) {
+            this.email_editTxt.setError("Der skal indtastes en E-mail.");
+            valid = false;
+        }
+        if (TextUtils.isEmpty(password)&& !TextUtils.isEmpty(email)) {
+            this.password_editTxt.setError("Der skal indtastes et password");
+            valid = false;
+        }
+
+        Log.d(TAG, "valideringEmail: validering returneres");
+        return valid;
+
+
+    }
+
+    public void guiLogind(){
+        if(mAuth.getCurrentUser() != null){
+            Intent myIntent = new Intent(Login_akt.this,Navigation_akt.class);
+            Login_akt.this.startActivity(myIntent);
+        }
+
+
+    }
+
+
+
 }
