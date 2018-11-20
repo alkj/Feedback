9a5a1121fc4e894e8203a70bd8754aa91d43dfd3
tahanynemer <tahanynemer3@hotmail.com>
2018-11-20 17:00:04 +0100

    rettesle

--- app/src/main/java/com/example/admin/feedback_app/uddyb_feedback.java
+++ app/src/main/java/com/example/admin/feedback_app/uddyb_feedback.java
@@ -8,6 +8,6 @@ public class uddyb_feedback extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.uddyb_feedback);
     }
 }
