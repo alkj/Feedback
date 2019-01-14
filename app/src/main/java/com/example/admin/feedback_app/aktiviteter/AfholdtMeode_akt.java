16a74af5701984cb250c3f17693231f7dc8180d6
Ahad2906 <s175128@student.dtu.dk>
2019-01-14 18:35:12 +0100

    afholdt møde, småt

--- app/src/main/java/com/example/admin/feedback_app/aktiviteter/AfholdtMeode_akt.java
+++ app/src/main/java/com/example/admin/feedback_app/aktiviteter/AfholdtMeode_akt.java
@@ -2,10 +2,19 @@ package com.example.admin.feedback_app.aktiviteter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.example.admin.feedback_app.Møde;
 import com.example.admin.feedback_app.R;
 
 public class AfholdtMeode_akt extends AppCompatActivity {
+
+    private Møde møde;
+
+    private TextView moedeNavn, moedeID;
+
+    private Button moedeInfo, overordnet_knap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
