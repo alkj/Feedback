9f97c788d3916ebd9cbe789c9142d6fa2a4291cd
nilsrasa <32563656+nilsrasa@users.noreply.github.com>
2019-01-17 10:50:24 +0100

    Begyndt at indføre viewpager til feedback

--- app/src/main/java/com/example/admin/feedback_app/aktiviteter/Overholdt_Giv_Feedback_akt.java
+++ app/src/main/java/com/example/admin/feedback_app/aktiviteter/Overholdt_Giv_Feedback_akt.java
@@ -1,6 +1,10 @@
 package com.example.admin.feedback_app.aktiviteter;
 
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,6 +18,22 @@ public class Overholdt_Giv_Feedback_akt extends AppCompatActivity implements Fee
 
     private String TAG = "feedbackfragmentet";
 
+    /**
+     * The number of pages (wizard steps) to show in this demo.
+     */
+    private static final int NUM_PAGES = 5;
+
+    /**
+     * The pager widget, which handles animation and allows swiping horizontally to access previous
+     * and next wizard steps.
+     */
+    private ViewPager viewPager;
+
+    /**
+     * The pager adapter, which provides the pages to the view pager widget.
+     */
+    private PagerAdapter pagerAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,31 +42,48 @@ public class Overholdt_Giv_Feedback_akt extends AppCompatActivity implements Fee
         String datoString = getIntent().getStringExtra("MØDEID");
         String stedString = getIntent().getStringExtra("MØDEIDdel");
 
-        fragmentFeedback = Feedback_frg.newInstance("spørgsmål 1 hvor gammel er du?");
-
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_feedback_container, fragmentFeedback).addToBackStack(null).commit();
-
-    }
-
-
-    @Override
-    public void onFragmentInteraction(int smiley, String tekstUddybning) {
-
-        Log.d(TAG, "onFragmentInteraction: " + "modtaget i aktivitet " + smiley + " og tekst " + tekstUddybning);
-
-        fragmentFeedback = Feedback_frg.newInstance("nyt spgrsmål");
-
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_feedback_container, fragmentFeedback).addToBackStack(null).commit();
-
+        // Instantiate a ViewPager and a PagerAdapter.
+        viewPager = findViewById(R.id.feedback_pager);
+        pagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
     }
 
     @Override
     public void onBackPressed() {
-
-        super.onBackPressed();
+        if (viewPager.getCurrentItem() == 0) {
+            // If the user is currently looking at the first step, allow the system to handle the
+            // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed();
+        } else {
+            // Otherwise, select the previous step.
+            viewPager.setCurrentItem(viewPager.getCurrentItem() - 1);
+        }
     }
 
+    @Override
+    public void onFragmentInteraction(int smiley, String tekstUddybning) {
+        Log.d("FRAGMENT", "Int: " + smiley + " and " + tekstUddybning);
+    }
 
+    /**
+     * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in
+     * sequence.
+     */
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return Feedback_frg.newInstance("spørgsmål " + position + " hvor gammel er du?");
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
+    }
 
 
 }
