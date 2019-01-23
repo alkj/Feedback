4efe86ec6cbd02feb71ce9cbed5739ba90f20b24
Ahad2906 <s175128@student.dtu.dk>
2019-01-23 21:33:40 +0100

    småt

--- app/src/main/java/com/example/admin/feedback_app/aktiviteter/AfholdtMeode_akt.java
+++ app/src/main/java/com/example/admin/feedback_app/aktiviteter/AfholdtMeode_akt.java
@@ -1,26 +1,15 @@
 package com.example.admin.feedback_app.aktiviteter;
 
-import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ExpandableListView;
 import android.widget.TextView;
 
 import com.example.admin.feedback_app.FeedbackSpørgsmål;
-import com.example.admin.feedback_app.FeedbackTilFirebase;
 import com.example.admin.feedback_app.Møde;
 import com.example.admin.feedback_app.PersonData;
 import com.example.admin.feedback_app.R;
 import com.example.admin.feedback_app.adaptere.SpørgsmålAdapter;
-import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.components.Legend;
-import com.github.mikephil.charting.data.PieData;
-import com.github.mikephil.charting.data.PieDataSet;
-import com.github.mikephil.charting.data.PieEntry;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -30,7 +19,7 @@ import java.util.Random;
 
 public class AfholdtMeode_akt extends AppCompatActivity {
 
-    private TextView mødenavn;
+    private TextView mødenavn, dato;
     private Møde møde;
 
 
@@ -49,6 +38,10 @@ public class AfholdtMeode_akt extends AppCompatActivity {
             }
         }
         mødenavn = findViewById(R.id.afholdt_mødenavn);
+        mødenavn.setText(møde.getNavn());
+
+        dato = findViewById(R.id.tvDato_Afholdt);
+        dato.setText(møde.getDato());
 
 
         ExpandableListView listView = findViewById(R.id.afhold_kommentarer);
